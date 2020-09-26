a0c660d7bbf180fcf414637705a31235b76e9231
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-09-26 16:03:51 -0400

    go client posts to ingestd

--- client/client.go
+++ client/client.go
@@ -3,39 +3,49 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
+	"time"
 
 	"github.com/joncrlsn/dque"
 	"github.com/stratoberry/go-gpsd"
 )
 
 const url = "https://ingest.jrcichra.dev"
+const database = "gps"
+const table = "gps"
+const timezone = "America/New_York"
+
+//gps record with hostname metadata
+type dbRecord struct {
+	*gpsd.TPVReport
+	Hostname string `json:"hostname"`
+}
 
 //gpsRecord - a basic GPS datapoint
 type gpsRecord struct {
 	Value gpsd.TPVReport
 }
 
-// gpsRecordBuilder - abstracts out a GPSRecord for dque to work
-func gpsRecordBuilder() interface{} {
-	return &gpsRecord{}
+// dbRecordBuilder - abstracts out a dbRecord for dque to work
+func dbRecordBuilder() interface{} {
+	return &dbRecord{}
 }
 
 func makeQueue() *dque.DQue {
 	qName := "gps_queue"
 	qDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	fmt.Println("qDir=", qDir)
+	log.Println("qDir=", qDir)
 	segmentSize := 50
 	if err != nil {
 		panic(err)
 	}
-	q, err := dque.NewOrOpen(qName, qDir, segmentSize, gpsRecordBuilder)
+	q, err := dque.NewOrOpen(qName, qDir, segmentSize, dbRecordBuilder)
 	if err != nil {
 		panic(err)
 	}
@@ -50,48 +60,100 @@ func makeGPS(hostname string, port int) *gpsd.Session {
 	return gps
 }
 
-func tpvToString(tpv *gpsd.TPVReport) string {
-	b, err := json.Marshal(tpv)
+func dbrToBytes(dbr *dbRecord) []byte {
+	b, err := json.Marshal(dbr)
 	if err != nil {
 		panic(err)
 	}
-	return string(b)
+	return b
+}
+
+func dbrToMap(dbr *dbRecord) map[string]interface{} {
+	m := make(map[string]interface{})
+	b, err := json.Marshal(dbr)
+
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		panic(err)
+	}
+	return m
 }
 
 func queueToPost(q *dque.DQue, h *http.Client) {
-	// Only dequeue if we could successfully POST
-	var tpv interface{}
-	var err error
+	for {
+		// Only dequeue if we could successfully POST
+		var t interface{}
+		var err error
 
-	if tpv, err = q.PeekBlock(); err != nil {
-		if err != dque.ErrEmpty {
-			log.Fatal("Error dequeuing item ", err)
+		if t, err = q.PeekBlock(); err != nil {
+			if err != dque.ErrEmpty {
+				log.Fatal("Error dequeuing item ", err)
+			}
+		}
+
+		// Convert it to map so we can rename a column
+		record := t.(*dbRecord)
+		m := dbrToMap(record)
+		tme := m["time"]
+		location, err := time.LoadLocation(timezone)
+		if err != nil {
+			panic(err)
+		}
+		ttime, err := time.Parse(time.RFC3339, tme.(string))
+		if err != nil {
+			panic(err)
+		}
+		stime := ttime.In(location).Format("2006-01-02 15:04:05")
+		m["gps_timestamp"] = stime
+		delete(m, "time")
+		b, err := json.Marshal(m)
+		if err != nil {
+			panic(err)
+		}
+		log.Println("POSTING:", string(b))
+		req, err := http.NewRequest("POST", url+"/"+database+"/"+table, bytes.NewBuffer(b))
+		req.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			panic(err)
+		}
+
+		resp, err := h.Do(req)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println("response Status:", resp.Status)
+		body, _ := ioutil.ReadAll(resp.Body)
+		log.Println("response Body:", string(body))
+		resp.Body.Close()
+		// Dequeue this variable now
+		_, err = q.Dequeue()
+		if err != nil {
+			panic(err)
 		}
 	}
+}
 
-	// Convert it to JSON and post
-	json := []byte(tpvToString(tpv.(*gpsd.TPVReport)))
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
-	req.Header.Set("Content-Type", "application/json")
-	if err != nil {
-		panic(err)
-	}
-
-	resp, err := h.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
-
-	// Dequeue this variable now
-	_, err = q.Dequeue()
-	if err != nil {
-		panic(err)
+func gpsdAlive(reset chan bool) {
+	const timeout = 15
+	ticker := time.NewTicker(timeout * time.Second)
+	cmd := exec.Command("sudo", "systemctl", "restart", "gpsd")
+	for {
+		select {
+		case <-ticker.C:
+			// GPSD did not send data and we need to reset it
+			log.Println("Attempting to restart gpsd...")
+			err := cmd.Run()
+			if err != nil {
+				log.Println(err)
+			}
+		case <-reset:
+			//Reset the ticker timer we got a point
+			ticker.Reset(timeout * time.Second)
+		}
 	}
 }
 
@@ -100,13 +162,27 @@ func main() {
 	gps := makeGPS("localhost", 2947)
 	q := makeQueue()
 	h := &http.Client{}
+	hostname, err := os.Hostname()
+	log.Println("hostname=", hostname)
+	if err != nil {
+		panic(err)
+	}
 	// Handle sending off HTTP posts
 	go queueToPost(q, h)
+	// Keep GPSD alive
+	gpsdchan := make(chan bool)
+	go gpsdAlive(gpsdchan)
 	// GPS loop
 	gps.AddFilter("TPV", func(r interface{}) {
 		// This anon function is called every time a new TPV value comes in, scoped this way so we can use q easily
+		gpsdchan <- true
 		tpv := r.(*gpsd.TPVReport)
-		fmt.Println("Location inserted into file queue:", tpvToString(tpv))
+		dbr := &dbRecord{tpv, hostname}
+		err := q.Enqueue(dbr)
+		if err != nil {
+			panic(err)
+		}
+		// log.Println("Location inserted into file queue:", string(dbrToBytes(dbr)))
 	})
 	done := gps.Watch()
 	<-done
